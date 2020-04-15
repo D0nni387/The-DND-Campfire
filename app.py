622f4bf50ad9bf9a1a1a57d92b1abc6a147a9e7a
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-04-15 20:17:08 +0100

    Linting to PEP8 & changed lower case app to APP

--- app.py
+++ app.py
@@ -3,28 +3,37 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-@app.route('/')
-def get_intro():   
-    return render_template("components/index.html")
 
-@app.route('/create')
+@APP.route('/')
+def get_intro():
+    """
+    Returns index page
+    """
+    return render_template("pages/index.html")
+
+
+@APP.route('/create')
 def create_character():
     return render_template("pages/create.html")
 
-@app.route('/insert_character', methods=['POST'])
+
+@APP.route('/insert_character', methods=['POST'])
 def insert_character():
     return redirect(url_for('get_party'))
 
-@app.route('/party')
+
+@APP.route('/party')
 def get_party():
     return render_template("pages/party.html")
 
-@app.route('/account')
+
+@APP.route('/account')
 def my_account():
     return render_template("pages/account.html")
 
+
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            debug=True)
+    APP.run(host=os.environ.get('IP'), port=os.environ.get('PORT'),
+            debug=os.environ.get('DEBUG'))
