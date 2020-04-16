1e15acc5d6c3d66729800bfca94b51d1b3b11a4c
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-04-16 19:07:48 +0100

    added listener for next button 1st choices

--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,40 @@
+const classId = document.getElementById("progress");
+const classChoice = document.getElementById("classList")
 
 
-  function classFetch() {
-  
-    fetch("http://dnd5eapi.co/api/classes/")
-        .then(response => response.json())
-        .then(classes => {
-            let classList = classes.results;
-            classList.forEach(classOp => {
+let baseURL = "http://dnd5eapi.co/api/classes/"
 
-                let classOption = document.createElement("option");
-                let className = document.createElement("p");
-                let name = document.createTextNode(classOp.name);
 
-                className.appendChild(name);
-                classOption.appendChild(className);
-                classOption.id = classOp.index;
-                classOption.classList.add("class");
-                document.getElementById("class").appendChild(classOption);
-            });
-            s
-        })
-        .catch(() => console.error());
+
+function classFetch() {
+
+  fetch(baseURL)
+    .then(response => response.json())
+    .then(classes => {
+      let classList = classes.results;
+      classList.forEach(classOp => {
+
+        let classOption = document.createElement("option");
+        let className = document.createElement("p");
+        let name = document.createTextNode(classOp.name);
+
+
+        className.appendChild(name);
+        classOption.appendChild(className);
+        classOption.id = classOp.index;
+        classOption.classList.add("class");
+        document.getElementById("classList").appendChild(classOption);
+      });
+
+    })
+    .catch(() => console.error());
 }
+
+classFetch();
+
+classId.addEventListener('click', () => {
+  id = classChoice.options[classChoice.selectedIndex].id;
+  console.log(id)
+  profFetch();
+});
+
