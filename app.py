9b2e25cb13db8054fb8a0e0395790cd3b775cc7c
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-04-22 15:32:29 +0100

    added additional fields to character JSON

--- app.py
+++ app.py
@@ -16,27 +16,44 @@ def get_intro():
 
 @APP.route('/create')
 def create_character():
+
+    """
+    Returns Create Page
+    """
+
     return render_template("pages/create.html")
 
 
 @APP.route('/insert_character', methods=['POST'])
 def insert_character():
+
+    """
+    Passes new character form data to JSON
+    """
     return jsonify({'name':request.form.get('name'),
-                    'class':request.form.get('class_list'), 
+                    'class':request.form.get('class_list'),
                     'hit_die':request.form.get('hit_die'),
                     'saving_throw1':request.form.get('saving_throw1'),
                     'saving_throw2':request.form.get('saving_throw2'),
                     'proficiency1':request.form.get('proficiency1'),
-                    'proficiency2':request.form.get('proficiency2')})
+                    'proficiency2':request.form.get('proficiency2'),
+                    'start_equip1':request.form.get('start_equip1'),
+                    'start_equip2':request.form.get('start_equip2')})
 
 
 @APP.route('/party')
 def get_party():
+    """
+    Returns Party Page
+    """
     return render_template("pages/party.html")
 
 
 @APP.route('/account')
 def my_account():
+    """
+    Returns Account Page
+    """
     return render_template("pages/account.html")
 
 
