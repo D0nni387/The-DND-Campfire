c60e0e564381cf5c42fadd46bccbc27eda03dbae
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-05-03 18:03:10 +0100

    updated registration to hash password and submit to db

--- app.py
+++ app.py
@@ -2,30 +2,16 @@ import os
 from flask import Flask, render_template, redirect, request, url_for, jsonify, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
 APP = Flask(__name__)
+
+""" To Debug Server Issue """
 APP.config['SECRET_KEY'] = 'Thisisasecretshhh'
 APP.config['MONGO_URI'] = 'mongodb+srv://d0nni3:Deadman87@campfire-sppig.azure.mongodb.net/Campfire?retryWrites=true&w=majority'
-BOOTSTRAP = Bootstrap(APP)
 MONGO = PyMongo(APP)
-
-
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=20)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=80)])
-    remember = BooleanField('remember me')
-
-class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid Email Address'), Length(max=40)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=20)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=80)])
 
 @APP.route('/')
 def get_intro():
@@ -82,33 +68,38 @@ def my_account():
     """
     return render_template("pages/account.html")
 
-@APP.route('/login', methods=['GET', 'POST'])
+@APP.route('/login', methods=['POST'])
 def login():
     """
     Returns Login Page
     """
-    form = LoginForm()
+    
 
-    if form.validate_on_submit():    
-        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+    if 'username' in session:
+        return 'Welcome back' + session['username']
 
-    return render_template("pages/login.html", form=form)
+
+    return render_template("pages/login.html")
 
 @APP.route('/register', methods=['GET', 'POST'])
 def register():
     """
     Returns Register Page
     """
-    form = RegisterForm()
-    user = MONGO.db.users
-    if form.validate_on_submit():
-        user_doc = {'username': request.form.get('username'),
-                    'password': request.form.get('password'),
-                    'email': request.form.get('email')}
-        user.insert_one(user_doc)
+    
+    if request.method == 'POST':
+        user = MONGO.db.users
+        active_user = user.find_one({'name' : request.form.get('username')})
 
-        return redirect(url_for('get_party'))
-    return render_template("pages/register.html", form=form)
+        if active_user is None:
+            hashpass = generate_password_hash(request.form.get('password'), 'sha256')
+            user.insert({'name' : request.form.get('username'), 'password' : hashpass, 'email' : request.form.get('email')})
+            session['username'] = request.form.get('username')
+            return redirect(url_for('get_intro'))
+
+        return 'That username exists'
+
+    return render_template("pages/register.html")
 
 
 if __name__ == '__main__':
