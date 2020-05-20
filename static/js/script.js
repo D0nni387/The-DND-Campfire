66867c46facaefff9f64978f54f77134aac3fae7
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-05-20 21:42:41 +0100

    fixed add to party not functioning

--- static/js/script.js
+++ static/js/script.js
@@ -8,7 +8,7 @@ const startEquip1 = document.getElementById("startEquip1");
 const startEquip2 = document.getElementById("startEquip2");
 const confirmEdit = document.getElementById("confirmEdit");
 const editButton = document.getElementById("editButton");
-const confirmAdd = document.getElementById("confirmAdd");
+const confirmAdd = document.getElementById("confirm");
 const phaseThree = document.getElementById("phaseThree");
 const classesList = document.getElementById("classList");
 const classChoice = document.getElementById("classList");
@@ -22,10 +22,10 @@ const initial = document.getElementById("phase");
 const hitDie = document.getElementById("hitDie");
 const load = document.getElementById("loading");
 const begin = document.getElementById("start");
-const name = document.getElementById("name")
+const nameField = document.getElementById("name");
 
 /* Global Variables */
-let edit = false
+let edit = false;
 
 /**
  * Takes the API data profList and produces select elements to DOM, 
@@ -42,18 +42,18 @@ const selectList = (profList, target, classes, naming) => {
     profList.forEach(item => {
       let profOption = document.createElement("option");
       let profName = document.createElement("p");
-      let name = document.createTextNode(item.name)
+      let name = document.createTextNode(item.name);
 
-      profName.appendChild(name)
-      profOption.appendChild(profName)
-      profOption.id = item.index
-      profOption.classList.add("skill")
-      target.appendChild(profOption)
+      profName.appendChild(name);
+      profOption.appendChild(profName);
+      profOption.id = item.index;
+      profOption.classList.add("skill");
+      target.appendChild(profOption);
     })
   } else if (classes) {
     profList.forEach(classOp => {
 
-      let i = 1
+      let i = 1;
       let classOption = document.createElement("option");
       let className = document.createElement("p");
       let name = document.createTextNode(classOp.name);
@@ -62,22 +62,22 @@ const selectList = (profList, target, classes, naming) => {
       classOption.appendChild(className);
       classOption.id = classOp.index;
       classOption.classList.add("class");
-      className.id = i
+      className.id = i;
       className.classList.add("classId")
       target.appendChild(classOption);
-      i++
+      i++;
     });
   } else {
     profList.forEach(item => {
       let profOption = document.createElement("option");
       let profName = document.createElement("p");
-      let name = document.createTextNode(item.item.name)
+      let name = document.createTextNode(item.item.name);
 
-      profName.appendChild(name)
-      profOption.appendChild(profName)
-      profOption.id = item.index
-      profOption.classList.add("skill")
-      target.appendChild(profOption)
+      profName.appendChild(name);
+      profOption.appendChild(profName);
+      profOption.id = item.index;
+      profOption.classList.add("skill");
+      target.appendChild(profOption);
     })
   }
 }
@@ -86,23 +86,23 @@ const selectList = (profList, target, classes, naming) => {
  * Fetches classes and populates select data
  */
 const classFetch = () => {
-  loadingWheel(true)
+  loadingWheel(true);
   fetch(`${baseURL}classes`)
     .then(response => response.json())
     .then(classes => {
 
       let classList = classes.results;
-      delete classList[5]
-      selectList(classList, classesList, true, false)
+      delete classList[5];
+      selectList(classList, classesList, true, false);
     })
     .catch(() => console.error());
 
-  loadingWheel(false, phaseOne)
+  loadingWheel(false, phaseOne);
 }
 
 classId.addEventListener('click', () => {
   id = classChoice.options[classChoice.selectedIndex].id;
-  phaseOne.classList.add("hide")
+  phaseOne.classList.add("hide");
   profFetch();
 
 });
@@ -111,26 +111,25 @@ classId.addEventListener('click', () => {
  * Fetches Proficiencies and populates select data
  */
 const profFetch = () => {
-  loadingWheel(true)
+  loadingWheel(true);
   fetch(`${baseURL}classes/${id}`)
     .then(response => response.json())
     .then(profs => {
 
-      let hit = profs.hit_die
-      hitDie.value = (`${hit}`)
+      let hit = profs.hit_die;
+      hitDie.value = (`${hit}`);
 
-      let saveOne = profs.saving_throws[0].name
-      savingThrow1.value = (`${saveOne}`)
+      let saveOne = profs.saving_throws[0].name;
+      savingThrow1.value = (`${saveOne}`);
 
-      let saveTwo = profs.saving_throws[1].name
-      savingThrow2.value = (`${saveTwo}`)
+      let saveTwo = profs.saving_throws[1].name;
+      savingThrow2.value = (`${saveTwo}`);
 
-      let profList = profs.proficiency_choices[0].from
-      selectList(profList, proficienciesOne, false, true)
-      selectList(profList, proficienciesTwo, false, true)
+      let profList = profs.proficiency_choices[0].from;
+      selectList(profList, proficienciesOne, false, true);
+      selectList(profList, proficienciesTwo, false, true);
     })
-
-  loadingWheel(false, phaseTwo)
+  loadingWheel(false, phaseTwo);
 }
 
 /**
@@ -142,23 +141,23 @@ const equipFetch = () => {
     .then(response => response.json())
     .then(equip => {
 
-      let startEquipOne = equip.starting_equipment[0].item.name
-      startEquip1.value = (`${startEquipOne}`)
+      let startEquipOne = equip.starting_equipment[0].item.name;
+      startEquip1.value = (`${startEquipOne}`);
 
       if (equip.starting_equipment[1]) {
-        let startEquipTwo = equip.starting_equipment[1].item.name
-        startEquip2.value = (`${startEquipTwo}`)
+        let startEquipTwo = equip.starting_equipment[1].item.name;
+        startEquip2.value = (`${startEquipTwo}`);
       } else {
-        startEquip2.value = (`No Secondary Equipment`)
+        startEquip2.value = (`No Secondary Equipment`);
       }
-      let profList = equip.choice_1[1].from
-      selectList(profList, equipmentChoice, false)
+      let profList = equip.choice_1[1].from;
+      selectList(profList, equipmentChoice, false);
     })
   loadingWheel(false, phaseThree)
   if (edit == true) {
-    confirmEdit.classList.remove("hide")
+    confirmEdit.classList.remove("hide");
   } else {
-    confirmAdd.classList.remove("hide")
+    confirmAdd.classList.remove("hide");
   }
 }
 
@@ -171,10 +170,10 @@ const equipFetch = () => {
  */
 const loadingWheel = (loading, container) => {
   if (loading != false) {
-    load.classList.remove("hide")
+    load.classList.remove("hide");
   } else {
     setTimeout(function () {
-      container.classList.remove("hide")
+      container.classList.remove("hide");
       load.classList.add("hide");
     }, 3000);
   }
@@ -183,26 +182,26 @@ const loadingWheel = (loading, container) => {
 /* On click Events */
 
 progress.addEventListener('click', () => {
-  idNum = classChoice.options[classChoice.selectedIndex].firstElementChild.id
-  phaseTwo.classList.add("hide")
+  idNum = classChoice.options[classChoice.selectedIndex].firstElementChild.id;
+  phaseTwo.classList.add("hide");
   equipFetch();
 });
 
 begin.addEventListener('click', () => {
-  if (name.value != "") {
-    initial.classList.add("hide")
+  if (nameField.value != "") {
+    initial.classList.add("hide");
     classFetch();
 
   } else {
     Swal.fire({
       icon: 'error',
       text: 'Please enter a character name!',
-    })
-  }
-})
+    });
+  };
+});
 
 editButton.addEventListener('click', () => {
-  editChoice.classList.add("hide")
-  editForm.classList.remove("hide")
-  edit = true
+  editChoice.classList.add("hide");
+  editForm.classList.remove("hide");
+  edit = true;
 })
